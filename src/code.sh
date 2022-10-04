f249f55c3ac38a3bb0b99db3421b7013882453ca
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-04 18:09:09 +0100

    Removed wider internet access from json (not needed currently). Run the Docker image saved in project.

--- src/code.sh
+++ src/code.sh
@@ -8,15 +8,17 @@ dx-download-all-inputs
 # download genome resources and Docker, decompress
 mkdir /home/dnanexus/genomeDir
 tar xvzf /home/dnanexus/in/genome_lib/*.tar.gz -C /home/dnanexus/genomeDir #transcript data from that release of gencode
-export STAR_REFERENCE="/home/dnanexus/genomeDir/*.plug-n-play/ctat_genome_lib_build_dir/ref_genome.fa.star.idx/"
+export STAR_REFERENCE=/home/dnanexus/genomeDir/*.plug-n-play/ctat_genome_lib_build_dir/ref_genome.fa.star.idx/
 tar xvzf /home/dnanexus/in/sf_docker/starfusion_*.tar.gz
+export DOCKER_IMAGE=/home/dnanexus/in/sf_docker/starfusion_*
 
 # create output directory
 mkdir -p out/starfusion_outputs
 
 mark-section "run starfusion"
 
-docker run -v `pwd`:/data --rm trinityctat/starfusion \
+docker run -v `pwd`:/data --rm \
+    "$DOCKER_IMAGE" \
     /usr/local/src/STAR-Fusion/STAR-Fusion \
     -J "$junctions" \
     --genome_lib_dir "$STAR_REFERENCE" \
