17d4b5aed2148388b9fa6cf52d02dc2eceda3402
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-04 14:35:49 +0100

    Changing Docker to run from saved image and added Docker file ID as input. Replaced fastq inputs with junction out file input. Changed notes to indicate STAR-Fusion 1.11.1 will be in use.

--- src/code.sh
+++ src/code.sh
@@ -9,18 +9,11 @@ mark-section "download inputs"
 dx cat "$genome_lib" | tar zxf -
 
 # download remaining inputs
-dx download "$left_fq" -o R1.fastq.gz
-dx download "$right_fq" -o R2.fastq.gz
+dx download "$junction" -o Chimeric.out.junction
 
 # create output directory
 mkdir -p out/starfusion_outputs
 lib_dir=$(find . -type d -name "GR*plug-n-play")
-
-mark-section "setup docker"
-
-# download starfusion docker this is August 2020 v1.9.1
-docker pull trinityctat/starfusion:1.9.1
-docker tag trinityctat/starfusion:1.9.1 trinityctat/starfusion:latest
 
 mark-section "run starfusion"
 
@@ -30,10 +23,9 @@ docker run -v `pwd`:/data --rm trinityctat/starfusion \
     --left_fq /data/R1.fastq.gz \
     --right_fq /data/R2.fastq.gz \
     --genome_lib_dir /data/"${lib_dir}/ctat_genome_lib_build_dir" \
-    -O /data/out/starfusion_outputs \
-    --FusionInspector inspect \
-    --examine_coding_effect \
-    --denovo_reconstruct
+    -O /data/out/starfusion_outputs 
+    # --examine_coding_effect \
+    # --denovo_reconstruct
 
 mark-section "upload outputs"
 
