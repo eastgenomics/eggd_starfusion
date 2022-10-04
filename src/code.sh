4bebb7c49201dce07b06fa535975787361e48ff8
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-04 17:58:32 +0100

    Setting STAR_REFERENCE to point to just the relevant bit of the reference library.

--- src/code.sh
+++ src/code.sh
@@ -8,6 +8,7 @@ dx-download-all-inputs
 # download genome resources and Docker, decompress
 mkdir /home/dnanexus/genomeDir
 tar xvzf /home/dnanexus/in/genome_lib/*.tar.gz -C /home/dnanexus/genomeDir #transcript data from that release of gencode
+export STAR_REFERENCE="/home/dnanexus/genomeDir/*.plug-n-play/ctat_genome_lib_build_dir/ref_genome.fa.star.idx/"
 tar xvzf /home/dnanexus/in/sf_docker/starfusion_*.tar.gz
 
 # create output directory
@@ -18,7 +19,7 @@ mark-section "run starfusion"
 docker run -v `pwd`:/data --rm trinityctat/starfusion \
     /usr/local/src/STAR-Fusion/STAR-Fusion \
     -J "$junctions" \
-    --genome_lib_dir "/home/dnanexus/genomeDir/ctat_genome_lib_build_dir" \
+    --genome_lib_dir "$STAR_REFERENCE" \
     --output_dir "/data/out/starfusion_outputs"
     # --examine_coding_effect \
     # --denovo_reconstruct
