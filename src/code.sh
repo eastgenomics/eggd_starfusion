f45d0e88d42f9b79817933eab116568a3134b6c7
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-04 16:58:55 +0100

    Decompressing StarFusion Docker; running with junctions not fastqs

--- src/code.sh
+++ src/code.sh
@@ -5,8 +5,10 @@ set -x
 
 mark-section "download inputs"
 
-# download genome resources and decompress
+# download genome resources and Docker, decompress
 dx cat "$genome_lib" | tar zxf -
+dx download "$sf_docker" 
+tar xvzf /home/dnanexus/in/sf_docker/starfusion_*.tar.gz
 
 # download remaining inputs
 dx download "$junction" -o Chimeric.out.junction
@@ -17,13 +19,11 @@ lib_dir=$(find . -type d -name "GR*plug-n-play")
 
 mark-section "run starfusion"
 
-# run starfusion
 docker run -v `pwd`:/data --rm trinityctat/starfusion \
     /usr/local/src/STAR-Fusion/STAR-Fusion \
-    --left_fq /data/R1.fastq.gz \
-    --right_fq /data/R2.fastq.gz \
+    -J "$junctions" \
     --genome_lib_dir /data/"${lib_dir}/ctat_genome_lib_build_dir" \
-    -O /data/out/starfusion_outputs 
+    --output_dir "/data/out/starfusion_outputs"
     # --examine_coding_effect \
     # --denovo_reconstruct
 
