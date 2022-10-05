353ea4574360ff63073dfb9a067339b7bdde3146
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-05 11:56:24 +0100

    Removed unnecessary (?) export of docker image ID. Replaced path to STAR-Fusion with the Docker's preset env variable, STAR_FUSION_HOME

--- src/code.sh
+++ src/code.sh
@@ -9,7 +9,6 @@ dx-download-all-inputs
 docker load -i /home/dnanexus/in/sf_docker/*
 # Get image id of the loaded docker
 DOCKER_IMAGE_ID=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^trinityctat/starfusion" | cut -d' ' -f2)
-export DOCKER_IMAGE_ID=$DOCKER_IMAGE_ID
 
 # download genome resources, decompress
 mkdir /home/dnanexus/genomeDir
@@ -22,8 +21,8 @@ mkdir -p out/starfusion_outputs
 mark-section "run starfusion"
 
 docker run -v "$(pwd)":/data --rm \
-    "$DOCKER_IMAGE_ID" \
-    /usr/local/src/STAR-Fusion/STAR-Fusion \
+    "${DOCKER_IMAGE_ID}" \
+    "${STAR_FUSION_HOME}"/STAR-Fusion \
     -J "/home/dnanexus/in/junction/*Chimeric.out.junction" \
     --genome_lib_dir "$STAR_REFERENCE" \
     --output_dir "/data/out/starfusion_outputs"
