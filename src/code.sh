d5d87be8b3bc02f9000db96c1e79e85388198547
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-05 16:07:28 +0100

    Trying to change where the STAR resources save to make them accessible to Docker

--- src/code.sh
+++ src/code.sh
@@ -3,17 +3,16 @@
 set -exo pipefail #if any part goes wrong, job will fail
 
 mark-section "download inputs"
-dx-download-all-inputs
+dx cat "$junction" | tar zxf -
+dx cat "$genome_lib" | tar zxf -
+
+# find the plug-n-play resources
+lib_dir=$(find . -type d -name "GR*plug-n-play")
 
 # load the Docker 
 docker load -i /home/dnanexus/in/sf_docker/*
 # Get image id of the loaded docker
 DOCKER_IMAGE_ID=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^trinityctat/starfusion" | cut -d' ' -f2)
-
-# download genome resources, decompress
-mkdir /home/dnanexus/genomeDir
-tar xvzf /home/dnanexus/in/genome_lib/*.tar.gz -C /home/dnanexus/genomeDir
-export STAR_REFERENCE=/home/dnanexus/genomeDir/*.plug-n-play
 
 # create output directory
 mkdir -p out/starfusion_outputs
@@ -24,7 +23,7 @@ docker run -v "$(pwd)" --rm \
     "${DOCKER_IMAGE_ID}" \
     STAR-Fusion \
     -J "/home/dnanexus/in/junction/*Chimeric.out.junction" \
-    --genome_lib_dir "$STAR_REFERENCE" \
+    --genome_lib_dir /"${lib_dir}/ctat_genome_lib_build_dir" \
     --output_dir "/home/dnanexus/out/starfusion_outputs"
     # --examine_coding_effect \
     # --denovo_reconstruct
