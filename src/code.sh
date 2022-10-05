886eda0b2cc121d2283c1df475f3b860cfcc9370
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-05 15:31:37 +0100

    Correcting mounted path, output dir and invocation for STAR-Fusion

--- src/code.sh
+++ src/code.sh
@@ -13,19 +13,19 @@ DOCKER_IMAGE_ID=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^trin
 # download genome resources, decompress
 mkdir /home/dnanexus/genomeDir
 tar xvzf /home/dnanexus/in/genome_lib/*.tar.gz -C /home/dnanexus/genomeDir
-export STAR_REFERENCE=/home/dnanexus/genomeDir/ref_genome.fa.star.idx/
+export STAR_REFERENCE=/home/dnanexus/genomeDir/GRCh38_gencode_v37_CTAT_lib_Mar012021.plug-n-play/ctat_genome_lib_build_dir/ref_genome.fa.star.idx
 
 # create output directory
 mkdir -p out/starfusion_outputs
 
 mark-section "run starfusion"
 
-docker run -v "$(pwd)":/data --rm \
+docker run -v "$(pwd)" --rm \
     "${DOCKER_IMAGE_ID}" \
     "${STAR_FUSION_HOME}"/STAR-Fusion \
     -J "/home/dnanexus/in/junction/*Chimeric.out.junction" \
     --genome_lib_dir "$STAR_REFERENCE" \
-    --output_dir "/data/out/starfusion_outputs"
+    --output_dir "/home/dnanexus/out/starfusion_outputs"
     # --examine_coding_effect \
     # --denovo_reconstruct
 
