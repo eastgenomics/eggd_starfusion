c20e6260ad0e726d9b32a0d27b884ecc97097ffe
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-05 10:58:54 +0100

    Using 'docker load' now instead of trying to tar (which does not work for Docker images)

--- src/code.sh
+++ src/code.sh
@@ -5,12 +5,15 @@ set -exo pipefail #if any part goes wrong, job will fail
 mark-section "download inputs"
 dx-download-all-inputs
 
-# download genome resources and Docker, decompress
+# download genome resources, decompress
 mkdir /home/dnanexus/genomeDir
 tar xvzf /home/dnanexus/in/genome_lib/*.tar.gz -C /home/dnanexus/genomeDir
 export STAR_REFERENCE=/home/dnanexus/genomeDir/ref_genome.fa.star.idx/
-tar xvzf /home/dnanexus/in/sf_docker/starfusion_*.tar.gz -C /home/dnanexus/in/sf_docker/starfusion
-export DOCKER_IMAGE="/home/dnanexus/in/sf_docker/starfusion"
+
+# load the Docker 
+docker load -i "$sf_docker"
+# Get image id of the loaded docker
+DOCKER_IMAGE_ID=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^trinityctat/starfusion" | cut -d' ' -f2)
 
 # create output directory
 mkdir -p out/starfusion_outputs
@@ -18,7 +21,7 @@ mkdir -p out/starfusion_outputs
 mark-section "run starfusion"
 
 docker run -v "$(pwd)":/data --rm \
-    "$DOCKER_IMAGE" \
+    "$DOCKER_IMAGE_ID" \
     /usr/local/src/STAR-Fusion/STAR-Fusion \
     -J "/home/dnanexus/in/junction/*Chimeric.out.junction" \
     --genome_lib_dir "$STAR_REFERENCE" \
