d63c5590ff16018b93a41646d0530b91c4ede757
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-05 17:22:41 +0100

    Put download-all-inputs back as more reliable. Making docker working dir be /home/dnanexus. Still unsure why ctat_genome_build_dir not being found.

--- src/code.sh
+++ src/code.sh
@@ -3,14 +3,14 @@
 set -exo pipefail #if any part goes wrong, job will fail
 
 mark-section "download inputs"
-dx download "$junction"
-dx cat "$genome_lib" | tar zxf -
+dx-download-all-inputs
+tar xvzf /home/dnanexus/in/genome_lib/*.tar.gz
 
 # find the plug-n-play resources
 lib_dir=$(find . -type d -name "GR*plug-n-play")
 
 # load the Docker 
-docker load -i /home/dnanexus/in/sf_docker/*
+docker load -i /home/dnanexus/in/sf_docker/*.tar.gz
 # Get image id of the loaded docker
 DOCKER_IMAGE_ID=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^trinityctat/starfusion" | cut -d' ' -f2)
 
@@ -19,11 +19,11 @@ mkdir -p out/starfusion_outputs
 
 mark-section "run starfusion"
 
-docker run -v "$(pwd)" --rm \
+docker run -v "$(pwd)":"$(pwd)" --rm \
     "${DOCKER_IMAGE_ID}" \
     STAR-Fusion \
     -J "/home/dnanexus/in/junction/*Chimeric.out.junction" \
-    --genome_lib_dir /"${lib_dir}/ctat_genome_lib_build_dir" \
+    --genome_lib_dir "${lib_dir}/ctat_genome_lib_build_dir" \
     --output_dir "/home/dnanexus/out/starfusion_outputs"
     # --examine_coding_effect \
     # --denovo_reconstruct
