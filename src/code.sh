dbd99d44498d241991cf72cb10641e01a3092f6e
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-06 12:01:23 +0100

    Move junction files around.

--- src/code.sh
+++ src/code.sh
@@ -2,20 +2,23 @@
 
 set -exo pipefail #if any part goes wrong, job will fail
 
-# download all inputs, untar the plug-n-play resources, and get
-# their path
+# download all inputs, untar the plug-n-play resources, and get its path
 mark-section "download inputs"
 dx-download-all-inputs
 tar xf /home/dnanexus/in/genome_lib/*.tar.gz -C /home/dnanexus/
 lib_dir=$(find . -type d -name "GR*plug-n-play")
+
+# Move all the junction files from subdirectories into one directory
+#find ~/in/junction -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/junction
+mkdir ~/junction
+mv ~/in/junction/* ~/junction
 
 # load the Docker and get its image ID
 docker load -i /home/dnanexus/in/sf_docker/*.tar.gz
 DOCKER_IMAGE_ID=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^trinityctat/starfusion" | cut -d' ' -f2)
 
 # get the sample name from the chimeric file
-sample_name=$(echo $junction_name | cut -d '_' -f 1)
-
+sample_name=$(echo "$junction_name" | cut -d '_' -f 1)
 
 # create output directory to move to
 mkdir -p /home/dnanexus/out/starfusion_outputs
@@ -25,7 +28,7 @@ mark-section "run starfusion"
 docker run -v "$(pwd)":/data --rm \
     "${DOCKER_IMAGE_ID}" \
     STAR-Fusion \
-    -J "/data/in/junction/*Chimeric.out.junction" \
+    -J "/data/junction/*Chimeric.out.junction" \
     --genome_lib_dir "/data/${lib_dir}/ctat_genome_lib_build_dir" \
     --output_dir "/data/out/starfusion_outputs"
     # --examine_coding_effect \
