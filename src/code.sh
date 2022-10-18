b64c9151a02ae6e61d99110e92dad67f0ba9e1d0
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-10-18 12:49:57 +0100

    Genome lib made less strict about inputs, so edited versions can be passed. Changed the output naming.

--- src/code.sh
+++ src/code.sh
@@ -5,8 +5,9 @@ set -exo pipefail #if any part goes wrong, job will fail
 # download all inputs, untar the plug-n-play resources, and get its path
 mark-section "download inputs"
 dx-download-all-inputs
-tar xf /home/dnanexus/in/genome_lib/*.tar.gz -C /home/dnanexus/
-lib_dir=$(find . -type d -name "GR*plug-n-play")
+tar xvzf /home/dnanexus/in/genome_lib/*.tar.gz -C /home/dnanexus/genomeDir
+#lib_dir=$(find . -type d -name "GR*plug-n-play")
+lib_dir=/home/dnanexus/genomeDir
 
 # load the Docker and get its image ID
 docker load -i /home/dnanexus/in/sf_docker/*.tar.gz
@@ -15,23 +16,22 @@ DOCKER_IMAGE_ID=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^trin
 # get the sample name from the chimeric file
 sample_name=$(echo "$junction_name" | cut -d '.' -f 1)
 
-# create output directory to move to
-final_dir="/home/dnanexus/out/${sample_name}_STAR-Fusion"
-mkdir -p "${final_dir}"
-
 mark-section "run starfusion"
 
 docker run -v "$(pwd)":/data --rm \
     "${DOCKER_IMAGE_ID}" \
     STAR-Fusion \
     -J "/data/in/junction/${sample_name}.chimeric.out.junction" \
-    --genome_lib_dir "/data/${lib_dir}/ctat_genome_lib_build_dir" \
+    --genome_lib_dir "/data/${lib_dir}" \
     --output_dir "/data/out"
 
-mark-section "move all output files to a sample-named directory, and rename them all to have the sample name too"
-
-cd ${final_dir}
-for f in $(ls); do mv "${f}" "${sample_name}.${f}"; done
+mark-section "move all output files to a named directory, and add sample names"
+# create output directory to move to
+final_dir="/home/dnanexus/out/${sample_name}_STAR-Fusion"
+mkdir -p "${final_dir}"
+# rename and move files
+find /home/dnanexus/out -type f -name "*" -printf "%f\n" | \
+xargs -I{} mv /home/dnanexus/out/{} "${final_dir}"/"${sample_name}"_{}
 
 mark-section "upload outputs"
 
